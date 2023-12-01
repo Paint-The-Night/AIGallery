4c62deb05624589fa406ac23ef9bc3069e906659
Paint The Night <3980690+Paint-The-Night@users.noreply.github.com>
2023-12-01 12:12:48 +1000

    updates

--- scripts/loadimage.js
+++ scripts/loadimage.js
@@ -9,7 +9,7 @@ document.addEventListener('DOMContentLoaded', function() {
                     document.getElementById('name').textContent = 'Name: ' + imageData.name;
                     document.getElementById('model').textContent = 'Model: ' + imageData.model;
                     document.getElementById('dateCreated').textContent = 'Date Created: ' + imageData.dateCreated;
-                    document.getElementById('description').textContent = 'Description: ' + imageData.description;
+                    document.getElementById('description').textContent = imageData.longDescription;
                 } else {
                     console.error('Image data not found for ID:', imageId);
                 }
@@ -23,33 +23,34 @@ document.addEventListener('DOMContentLoaded', function() {
             .then(response => response.json())
             .then(images => {
                 const galleryContainer = document.getElementById('gallery-container');
-    
+
                 images.forEach(image => {
                     const div = document.createElement('div');
                     div.className = 'gallery-item';
-                    div.innerHTML = `
+                    const anchor = document.createElement('a');
+                    anchor.href = `/works/${image.id}.html`;
+
+                    anchor.innerHTML = `
                         <img src="${image.thumbnail}" alt="${image.name}" class="gallery-thumbnail">
                         <h2>${image.name}</h2>
-                        <p>Model: ${image.model}</p>
-                        <p>Date Created: ${image.dateCreated}</p>
-                        <p>${image.description}</p>
+                        <p>${image.shortDescription}</p>
                     `;
+
+                    div.appendChild(anchor);
                     galleryContainer.appendChild(div);
                 });
             })
             .catch(error => console.error('Error:', error));
     }
-    
 
-    // Check for the gallery container and load gallery if it exists
+    // Check if the gallery container exists and load the gallery
     if (document.getElementById('gallery-container')) {
         generateGallery();
     }
 
-    // Check for an image ID and load individual image data if it exists
+    // Check if an image ID is specified and load individual image data
     const imageId = document.body.getAttribute('data-image-id');
     if (imageId) {
         loadImageData(imageId);
     }
 });
-
